9892bc8589a861b97871002d3b9de336e7bc97ce
diva-perez <diperez@uiowa.edu>
2021-04-07 12:00:59 -0500

    Update to get the background on the MainWindow
    Background is a free-use image from Pixabay user ID 12019 (inactive account)
    
    MainWindow is now maximized to the user's screen size

--- Battleship/src/MainWindow.java
+++ Battleship/src/MainWindow.java
@@ -1,16 +1,17 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class MainWindow extends JFrame {
 
     public static void main(String[] args) {
         MainWindow window = new MainWindow();
+        window.setExtendedState(JFrame.MAXIMIZED_BOTH);
     }
 
     public MainWindow() {
         super("Battleship");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setContentPane(new Background("Background.jpg"));
-        setExtendedState(JFrame.MAXIMIZED_BOTH);
         pack();
 
         MainMenu menu = new MainMenu(this);
