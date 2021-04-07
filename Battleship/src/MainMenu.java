bb44af012d60f1a08aa42eea778ee29cbe1efdf1
diva-perez <diperez@uiowa.edu>
2021-04-07 13:46:50 -0500

    Finished the layout of the MainMenuPanel
    Added two buttons, no funcitonality yet

--- Battleship/src/MainMenu.java
+++ Battleship/src/MainMenu.java
@@ -17,6 +17,28 @@ public class MainMenu extends JPanel {
         title.setFont(new Font("Lucida Bright", Font.BOLD, 100));
         title.setBorder(BorderFactory.createEmptyBorder(10, 0, 50, 0));
         add(title, BorderLayout.NORTH);
+        
+
+        // start button
+        JButton start = new JButton("Start Game");
+        start.setFont(new Font("Arial", Font.PLAIN, 40));
+        start.setAlignmentX(Component.CENTER_ALIGNMENT);
+        // need listener
+
+        //instruction button
+        JButton instruction = new JButton("Instructions");
+        instruction.setFont(new Font("Arial", Font.PLAIN, 40));
+        instruction.setAlignmentX(Component.CENTER_ALIGNMENT);
+        // need listener
+
+        // menu panel
+        JPanel menuPanel = new JPanel();
+        menuPanel.setOpaque(false);
+        menuPanel.setLayout(new BoxLayout(menuPanel, BoxLayout.PAGE_AXIS));
+        menuPanel.add(start);
+        menuPanel.add(Box.createRigidArea(new Dimension(10, 10))); // spacer between buttons
+        menuPanel.add(instruction);
+        add(menuPanel, BorderLayout.SOUTH);
     }
 
 }
