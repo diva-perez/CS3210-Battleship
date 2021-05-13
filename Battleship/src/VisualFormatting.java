34f923cc079c5447a7b60948603983856e698dc8
Dan Burleson <daniel-burleson@uiowa.edu>
2021-05-13 11:45:41 -0500

    formatting

---
+++ Battleship/src/VisualFormatting.java
@@ -0,0 +1,7 @@
+import java.awt.*;
+
+public class VisualFormatting {
+    public static Font headings1 = new Font("Serif", Font.BOLD, 100);
+    public static Font headings2 = new Font("Arial", Font.BOLD, 50);
+    public static Font buttons = new Font("Arial", Font.PLAIN, 40);
+}
